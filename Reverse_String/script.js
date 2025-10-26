048acb8a88a7c5bec78bfb3695342047f5b716f8
anshul shakya <anshulshakya0605@gmail.com>
2025-10-26 20:53:01 +0530

    string reverse

--- Reverse_String/script.js
+++ Reverse_String/script.js
@@ -1,5 +1,5 @@
-let myStr = "Ram";
+let myStr = "Ram Siya Ji";
 
-const reverseString = my.slice('').reverse().join('')
+const reverseString = myStr.split(' ').reverse().join(' ')
 console.log("my String:", reverseString);
 
