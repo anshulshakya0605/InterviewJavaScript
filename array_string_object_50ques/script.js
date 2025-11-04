6241f3778f2dde98118a26fecf298f7076e3d264
ramanshakyars <ramanshakyars12@gmail.com>
2025-11-05 00:55:20 +0530

    solving question on arrays

---
+++ array_string_object_50ques/script.js
@@ -0,0 +1,56 @@
+// 1. Array-based Coding (15 Questions)
+// console.log(Math.max(...studentsMarks));
+// const studentsMarks = [43, 43, 65, 76, 86];
+
+////// need to read about sort method   .sort
+
+
+
+// let maxMarks = studentsMarks[0];
+
+// for (let i = 1; i < studentsMarks.length; i++) {
+//     if (studentsMarks[i] > maxMarks) {
+//         maxMarks = studentsMarks[i]
+//     }
+// }
+// console.log(maxMarks);
+
+// Find the minimum number in an array using a loop.
+
+
+// let minMarks = studentsMarks[0];
+// for(let i = 1; i < studentsMarks.length;i++){
+//     if(studentsMarks[i]< minMarks){
+//         minMarks = studentsMarks[i];
+//     }
+// }
+//  console.log(minMarks);
+//   console.log(Math.min(...studentsMarks));
+
+// Reverse an array without using .reverse().
+
+const studentsMarks = [43, 43, 65, 76, 86];
+// console.log(studentsMarks.reverse());
+
+// Reverse an array without using .reverse().
+
+// let reversedArray = [];
+
+// for (let i = studentsMarks.length - 1; i >= 0; i--) {
+//     reversedArray.push(studentsMarks[i]);
+// }
+// console.log(reversedArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
