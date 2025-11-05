3c1254f8f3c563cce2124a23a3106f95bd724492
anshulshakya0605 <anshulshakya0605@gmail.com>
2025-11-05 18:43:23 +0530

    solving questions of array_string_object_50ques

--- array_string_object_50ques/script.js
+++ array_string_object_50ques/script.js
@@ -43,14 +43,90 @@ const studentsMarks = [43, 43, 65, 76, 86];
 
 
 
+// Count how many even and odd numbers are in an array.
+//solve with for loop
+
+// function countEvenOdd(arr) {
+//     let evenNum = 0;
+//     let oddNum = 0;
+//     for(let i = 0; i < arr.length; i++ ){
+//         if (arr[i] %2 === 0) {
+//             evenNum++
+//         }else {
+//             oddNum++
+//         }
+//     }
+//     console.log("Total even numbers: " + evenNum);
+//     console.log("Total odd numbers: " + oddNum);
+// }
+
+const studentsRoll = [12,21,53,1,9,4,8,3,75,14]
+// countEvenOdd(studentsRoll)
+
+// solve with filter method
+ 
+
+// function countEvenOddFilterMethod(arr) {
+//     let evenNum = arr.filter((num ) => num % 2 === 0).length
+//     let oddNum = arr.filter((num ) => num % 2 === 1).length
+
+//     console.log("Total even numbers:" + evenNum);
+//     console.log("Total odd numbers:" + oddNum); 
+// }
+
+// countEvenOddFilterMethod(studentsRoll)
 
 
 
 
 
+// Check if an array is sorted (ascending order).
+
+// const sortedArray = studentsRoll.sort((a, b) => a-b)
+// console.log(sortedArray);  //    ****   ascending order   ****
+
+
+// const sortedArray = studentsRoll.sort((a, b) => b-a)
+// console.log(sortedArray);  //    ****   descending order   ****
 
 
 
 
+// Find the sum of all numbers in an array using a for loop.
+
+// let sumOfAllNumbers = 0;
+
+// for(let i = 0; i < studentsRoll.length; i++) {
+//     sumOfAllNumbers += studentsRoll[i];
+// }
+// console.log(sumOfAllNumbers);
+
+// const sumOfNumberUsingReduce = studentsRoll.reduce((acc, curr) => acc + curr)
+
+// console.log(sumOfNumberUsingReduce);
 
 
+
+
+// Remove duplicate elements from an array.
+
+// using Set method
+const arrayOfDuplicateNum = [1,2,3,4,5,6,3,2,1,5,4,7,8,9,1,31];
+
+// // const uniqueArray = [... new Set(arrayOfDuplicateNum)];
+
+// // using filter method
+// const uniqueArray = arrayOfDuplicateNum.filter((num, index) => arrayOfDuplicateNum.indexOf(num) == index)
+
+// console.log(uniqueArray);
+
+
+
+
+// Merge two arrays and sort the result in ascending order.
+
+// const mergedArray = studentsRoll.concat(arrayOfDuplicateNum);
+// console.log(mergedArray.sort((a, b) => a - b));
+
+// const mergedArray = [...arrayOfDuplicateNum, ...studentsRoll];
+// console.log(mergedArray.sort((a, b) => a- b));
